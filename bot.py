94b8a12a81e3b977ee7111221e2a5a091833ee1e
korgn <69055432+korgn@users.noreply.github.com>
2023-02-23 13:40:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-import asyncio
-import time
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
+import os
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
 
 API_TOKEN = '6102750853:AAHOZ95KPNLsYKA9AY_D6ef-GJTlBEedG2E'
 
@@ -10,8 +11,20 @@ API_TOKEN = '6102750853:AAHOZ95KPNLsYKA9AY_D6ef-GJTlBEedG2E'
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
+TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 user_messages = {}
 
@@ -22,7 +35,7 @@ async def send_help(message: types.Message):
 
 @dp.message_handler(commands=['s'])
 async def send_help(message: types.Message):
-    await bot.send_message(message.chat.id, "Смоктати я не вмію, але це гарно робить Ром... Алукард.\n\nhttps://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    await bot.send_message(message.chat.id, "Смоктати я не вмію, але це гарно робить Ром... Алукард.")
 
 @dp.message_handler(content_types=['text', 'animation', 'sticker'])
 async def handle_message(message: types.Message):
@@ -55,10 +68,11 @@ async def handle_new_chat_member(message: types.Message):
     await bot.send_message(message.chat.id, f"Привіт, @{message.new_chat_members[0].username}!")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    asyncio.run(dp.start_webhook(
-        listen="0.0.0.0",
-        port=port,
-        url_path=API_TOKEN,
-        webhook_url=f"https://myfb.com/{API_TOKEN}"
-    ))
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
