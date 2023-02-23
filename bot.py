f3bd133e18c43f52c6a1448346c53a2b0a69f1d2
korgn <69055432+korgn@users.noreply.github.com>
2023-02-23 12:42:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,31 @@
+import asyncio
 import time
-import telegram
-import os
-PORT = int(os.environ.get('PORT', 5000))
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ParseMode
 
-bot = telegram.Bot(token='6102750853:AAHOZ95KPNLsYKA9AY_D6ef-GJTlBEedG2E')
-TOKEN = '6102750853:AAHOZ95KPNLsYKA9AY_D6ef-GJTlBEedG2E'
+API_TOKEN = '6102750853:AAHOZ95KPNLsYKA9AY_D6ef-GJTlBEedG2E'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
 user_messages = {}
 
-def send_help(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id, "Мої можливості досить прості. Я звичайнісенький бот антіспам, без налаштувань чи щось такого, і я мучу за спам, після чого кличу админів командою репорт. \n\n[Розроблено спеціально для чату Бестіарій, і розраховано виключно на працю в ньому.]")
+@dp.message_handler(commands=['h'])
+@dp.message_handler(commands=['можливості'])
+async def send_help(message: types.Message):
+    await bot.send_message(message.chat.id, "Мої можливості досить прості. Я звичайнісенький бот антіспам, без налаштувань чи щось такого, і я мучу за спам, після чого кличу админів командою репорт. \n\n[Розроблено спеціально для чату Бестіарій, і розраховано виключно на працю в ньому.]")
 
-def smoktat(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id, "Смоктати я не вмію, але це гарно робить Ром... Алукард.")
-    
-    #https://www.youtube.com/watch?v=dQw4w9WgXcQ
+@dp.message_handler(commands=['s'])
+async def send_help(message: types.Message):
+    await bot.send_message(message.chat.id, "Смоктати я не вмію, але це гарно робить Ром... Алукард.\n\nhttps://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
-def handle_message(update, context):
-    message = update.message
+@dp.message_handler(content_types=['text', 'animation', 'sticker'])
+async def handle_message(message: types.Message):
     user_id = message.from_user.id
     username = message.from_user.username
 
@@ -34,28 +40,25 @@ def handle_message(update, context):
     if len(user_messages[user_id]) >= 5:
         if user_messages[user_id][-1] - user_messages[user_id][-3] <= 5:
             try:
-                context.bot.restrict_chat_member(chat_id, user_id, until_date=0)
-                context.bot.send_message(chat_id, f"@{username}({user_id}) замучений назавжди")
-                context.bot.send_message(chat_id, f"/report @{username} [{user_id}] був замучений задля припинення спаму, дії далі вирішите самі.")
-            except: 
-                context.bot.send_message(chat_id, f"Помилка, скоріше за усе вона полягає в тому, що спамить адмін.")
+                await bot.restrict_chat_member(message.chat.id, user_id, until_date=0)
+                await bot.send_message(message.chat.id, f"@{username}({user_id}) замучений назавжди")
+                await bot.send_message(message.chat.id, f"/report @{username} [{user_id}] був замучений задля припинення спаму, дії далі вирішите самі.")
+            except:
+                await bot.send_message(message.chat.id, f"Помилка, скоріше за усе вона полягає в тому, що спамить адмін.")
             try:
                 del user_messages[user_id]
             except:
                 pass
 
-from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
+@dp.message_handler(content_types=['new_chat_members'])
+async def handle_new_chat_member(message: types.Message):
+    await bot.send_message(message.chat.id, f"Привіт, @{message.new_chat_members[0].username}!")
 
-updater = Updater(token='your_bot_token_here', use_context=True)
-dispatcher = updater.dispatcher
-
-dispatcher.add_handler(CommandHandler(['можливості', 'h'], send_help))
-dispatcher.add_handler(CommandHandler('s', smoktat))
-dispatcher.add_handler(MessageHandler(Filters.text | Filters.animation | Filters.sticker, handle_message))
-dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members,))
-
-updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
-updater.idle()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    asyncio.run(dp.start_webhook(
+        listen="0.0.0.0",
+        port=port,
+        url_path=API_TOKEN,
+        webhook_url=f"https://myfb.com/{API_TOKEN}"
+    ))
