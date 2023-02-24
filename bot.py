2d20cec36b0733b8322127640d65a6e7b57a3e82
korgn <69055432+korgn@users.noreply.github.com>
2023-02-24 16:44:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,41 +36,16 @@ def handle_message(message):
         if user_messages[user_id][-1] - user_messages[user_id][-5] <= 5:
             try:
                 # Заборонити користувача на 5 хвилин
-                bot.restrict_chat_member(message.chat.id, user_id, until_date=0)
-
-                # Створення кнопок для зняття муту та бану
-                markup = types.InlineKeyboardMarkup(row_width=2)
-                unmute_button = types.InlineKeyboardButton("Зняти мут", callback_data=f"unmute:{user_id}")
-                ban_button = types.InlineKeyboardButton("Забанити", callback_data=f"ban:{user_id}")
-                markup.add(unmute_button, ban_button)
+                bot.restrict_chat_member(message.chat.id, user_id, until_date=mute_time)
 
                 # Відправлення повідомлення про мут та кнопок для зняття муту та бану
-                bot.send_message(message.chat.id, f"@{username} [{user_id}] був замучений задля припинення спаму.", reply_markup=markup)
+                bot.send_message(message.chat.id, f"@{username} [{user_id}] був замучений задля припинення спаму.")
             except: 
                 bot.send_message(message.chat.id, f"@{username} [{user_id}] повинен був бути замучений, але трапилася опимлка, скоріше за усе вона полягає у тому, що цей користувач адміністратор чату.")
             try:
                 del user_messages[user_id]
             except:
                 pass
-
-@bot.callback_query_handler(func=lambda call: True)
-def handle_callback(call):
-    user_id = call.data.split(":")[1]
-
-    # Перевірити чи є користувач адміністратором
-    chat_member = bot.get_chat_member(call.message.chat.id, call.from_user.id)
-    if not chat_member.is_chat_admin():
-        bot.answer_callback_query(call.id, text="Тільки адміністратори можуть виконувати цю дію.")
-        return
-
-    if call.data.startswith("unmute"):
-        # Зняти мут з користувача
-        bot.restrict_chat_member(call.message.chat.id, user_id, until_date=0, can_send_messages=True)
-        bot.answer_callback_query(call.id, text="Мут знято")
-    elif call.data.startswith("ban"):
-        # Забанити користувача
-        bot.ban_chat_member(call.message.chat.id, user_id)
-        bot.answer_callback_query(call.id, text="Користувач забанений")
 
 
 bot.polling()
