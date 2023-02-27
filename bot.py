ea0763f70e0945fffaa2e443ce51836507a3db1f
korgn <69055432+korgn@users.noreply.github.com>
2023-02-27 17:53:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -108,10 +108,10 @@ def handle_callback_query(call):
 
     if call.data.startswith("ban"):
         bot.kick_chat_member(call.message.chat.id, user_id)
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"@{username} [{user_id}] був забанений.")
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"[{user_id}] був забанений.")
     elif call.data.startswith("unmute"):
         bot.restrict_chat_member(call.message.chat.id, user_id, can_send_messages=True)
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"@{username} [{user_id}] був розмучений.")
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"[{user_id}] був розмучений.")
 
     bot.answer_callback_query(call.id)
     bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=None)
